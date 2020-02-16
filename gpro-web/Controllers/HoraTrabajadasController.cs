168ae5783dfd5e09fbc4beab65c690ac4a093ee3
licaceres <licaceres04@gmail.com>
2020-02-16 12:47:37 -0300

    Empezado HoraTrabajada

---
+++ gpro-web/Controllers/HoraTrabajadasController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using gpro_web.Models;
+using gpro_web.Dtos;
+using AutoMapper;
+using gpro_web.Helpers;
+using Microsoft.Extensions.Options;
+
+namespace gpro_web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HoraTrabajadasController : ControllerBase
+    {
+        private readonly gpro_dbContext _context;
+        private IMapper _mapper;
+        private readonly AppSettings _appSettings;
+
+        public HoraTrabajadasController(gpro_dbContext context, IMapper mapper,
+            IOptions<AppSettings> appSettings)
+        {
+            _context = context;
+            _mapper = mapper;
+            _appSettings = appSettings.Value;
+
+        }
+
+        // GET: api/HoraTrabajadas
+        [HttpGet]
+        public IEnumerable<HoraTrabajada> GetHoraTrabajada()
+        {
+            return _context.HoraTrabajada;
+        }
+
+        // GET: api/HoraTrabajadas/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetHoraTrabajada([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var horaTrabajada = _mapper.Map<HoraTrabajadasDto>(await _context.HoraTrabajada.FindAsync(id));
+
+            if (horaTrabajada == null)
+            {
+                return NotFound();
+            }
+
+            
+
+            return Ok(horaTrabajada);
+        }
+
+        // PUT: api/HoraTrabajadas/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHoraTrabajada([FromRoute] int id, [FromBody] HoraTrabajada horaTrabajada)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != horaTrabajada.ProyectoIdProyecto)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(horaTrabajada).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HoraTrabajadaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/HoraTrabajadas
+        [HttpPost]
+        public async Task<IActionResult> PostHoraTrabajada([FromBody] HoraTrabajada horaTrabajada)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.HoraTrabajada.Add(horaTrabajada);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (HoraTrabajadaExists(horaTrabajada.ProyectoIdProyecto))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetHoraTrabajada", new { id = horaTrabajada.ProyectoIdProyecto }, horaTrabajada);
+        }
+
+        // DELETE: api/HoraTrabajadas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHoraTrabajada([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var horaTrabajada = await _context.HoraTrabajada.FindAsync(id);
+            if (horaTrabajada == null)
+            {
+                return NotFound();
+            }
+
+            _context.HoraTrabajada.Remove(horaTrabajada);
+            await _context.SaveChangesAsync();
+
+            return Ok(horaTrabajada);
+        }
+
+        private bool HoraTrabajadaExists(int id)
+        {
+            return _context.HoraTrabajada.Any(e => e.ProyectoIdProyecto == id);
+        }
+    }
+}
---
+++ gpro-web/Services/HoraTrabajadaService.cs
@@ -0,0 +1,17 @@
+﻿using gpro_web.Helpers;
+using gpro_web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gpro_web.Services
+{
+    public interface IHoraTrabajadaService
+    {
+        List<Object> HorasPorProyecto(int idProyecto);
+        List<Object> HorasPorRecurso(int idEmpleado, DateTime inicio, DateTime fin);
+
+
+    }
+}
