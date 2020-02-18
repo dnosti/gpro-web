f2598ed48c757ff5a81c889406d455b2003df903
juanmavillarraza <juan.villarraza@gmail.com>
2020-02-18 11:58:05 -0300

    minors fix for layout

--- gpro-web/ClientApp/src/views/panel/index.js
+++ gpro-web/ClientApp/src/views/panel/index.js
@@ -21,22 +21,16 @@ const menu = [{
     icon: 'plus-circle'
   }],
 }, {
-  group: 'Clientes',
+  group: 'Gestión',
   items: [{
     path: '/clientes',
     label: 'ABM Clientes',
-    icon: 'user'
-  }]
-}, {
-  group: 'Empleados',
-  items: [{
+    icon: 'solution'
+  }, {
     path:'/empleados',
     label: 'ABM Empleados',
     icon: 'team'
-  }]
-}, {
-  group: 'Usuarios',
-  items: [{
+  }, {
     path: '/usuarios',
     label: 'ABM Usuarios',
     icon: 'user'
@@ -130,9 +124,11 @@ class Panel extends Component {
     const { currentUser } = this.state;
     return (
       <Header align='right' style={{ paddingRight: '15px' }}>
-        <Tag color='geekblue' hidden={isMobile}><Icon type='user' /> {currentUser.username}</Tag>
-        <Tag color='geekblue' style={{ marginRight: '25px' }} hidden={isMobile}><Icon type='tag' /> {currentUser.rol}</Tag>
-        <Button type='primary' breakpoint='sm' hidden={isMobile} onClick={this.logOut}>Cerrar sesión</Button>
+        <Tag color='geekblue' hidden={isMobile}><Icon type='user' /> Usuario: {currentUser.username}</Tag>
+        <Tag color='geekblue' style={{ marginRight: '25px' }} hidden={isMobile}><Icon type='tag' /> Rol: {currentUser.rol}</Tag>
+        <Button icon='logout' onClick={this.logOut} breakpoint='sm' hidden={isMobile}>
+          Cerrar sesión
+        </Button>
         <div hidden={!isMobile}>
           <Dropdown overlay={this.menuMovil} trigger={['click']}>
             <Button type='primary' className='ant-dropdown-link' shape='circle' icon='user' onClick={this.logOut} />
