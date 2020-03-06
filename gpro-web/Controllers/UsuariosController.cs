6b4e954bf708f802defa90bb50fb066385fd5ac6
dardonosti <dardonostiok@gmail.com>
2020-03-06 10:54:34 -0300

    [agregado] GetAll en Usuarios: Controller y Service

--- gpro-web/Controllers/UsuariosController.cs
+++ gpro-web/Controllers/UsuariosController.cs
@@ -19,175 +19,180 @@ using System.Security.Claims;
 
 namespace gpro_web.Controllers
 {
-    [Authorize]
-    [ApiController]
-    [Route("[controller]")]
-    public class UsuariosController : ControllerBase
+  [Authorize]
+  [ApiController]
+  [Route("[controller]")]
+  public class UsuariosController : ControllerBase
+  {
+    private IUsuarioService _userService;
+    private IMapper _mapper;
+    private readonly AppSettings _appSettings;
+
+    public UsuariosController(
+        IUsuarioService userService,
+        IMapper mapper,
+        IOptions<AppSettings> appSettings)
     {
-        private IUsuarioService _userService;
-        private IMapper _mapper;
-        private readonly AppSettings _appSettings;
+      _userService = userService;
+      _mapper = mapper;
+      _appSettings = appSettings.Value;
+    }
 
-        public UsuariosController(
-            IUsuarioService userService,
-            IMapper mapper,
-            IOptions<AppSettings> appSettings)
-        {
-            _userService = userService;
-            _mapper = mapper;
-            _appSettings = appSettings.Value;
-        }
+    [AllowAnonymous]
+    [HttpPost("authenticate")]
+    public IActionResult Authenticate([FromBody]UserDto userDto)
+    {
+      var user = _userService.Authenticate(userDto.Username, userDto.Password);
 
-        [AllowAnonymous]
-        [HttpPost("authenticate")]
-        public IActionResult Authenticate([FromBody]UserDto userDto)
-        {
-            var user = _userService.Authenticate(userDto.Username, userDto.Password);
+      if (user == null)
+        return BadRequest(new { message = "Username or password is incorrect" });
 
-            if (user == null)
-                return BadRequest(new { message = "Username or password is incorrect" });
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(new Claim[]
+          {
                     new Claim(ClaimTypes.Name, user.Id.ToString()),
                     new Claim(ClaimTypes.Role, user.IdRolNavigation.Rol1),
-                }),
-                Expires = DateTime.UtcNow.AddDays(7),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            var tokenString = tokenHandler.WriteToken(token);
+          }),
+        Expires = DateTime.UtcNow.AddDays(7),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+      };
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      var tokenString = tokenHandler.WriteToken(token);
 
-            // return basic user info (without password) and token to store client side
-            return Ok(new
-            {
-                Id = user.Id,
-                Username = user.Username,
-                IdRol = user.IdRol,
-                Rol = user.IdRolNavigation.Rol1,
-                IdEmpleado = user.IdEmpleado,
-                Token = tokenString
-            });
-        }
-
-        [AllowAnonymous]
-        [HttpPost("register")]
-        public IActionResult Register([FromBody]UserDto userDto)
-        {
-            // map dto to entity
-            var user = _mapper.Map<Usuario>(userDto);
-
-            try
-            {
-                // save 
-                _userService.Create(user, userDto.Password);
-                return Ok();
-            }
-            catch (AppException ex)
-            {
-                // return error message if there was an exception
-                return BadRequest(new { message = ex.Message });
-            }
-        }
-
-        [Authorize (Roles = "Admin, PM")]
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var users = _userService.GetAll();
-            var userDtos = _mapper.Map<IList<UserDto>>(users);
-            return Ok(userDtos);
-        }
-
-        //[Authorize (Roles = "Admin")]
-        [AllowAnonymous]
-        [HttpGet("apynom/{dato}")]
-        public IActionResult GetByApyNom([FromRoute] string dato)
-        {
-            var users = _userService.GetByDato(dato);
-            var usersDto = _mapper.Map<IList<UserEmplDto>>(users);
-            if (usersDto.Count() == 0)
-            {
-                return NotFound();
-            }
-            return Ok(usersDto);
-        }
-
-        [AllowAnonymous]
-        [HttpGet("dni/{dni}")]
-        public IActionResult GetByDni(int dni)
-        {
-            var user = _userService.GetByDni(dni);
-            var usersDto = _mapper.Map<IList<UserEmplDto>>(user);
-
-            if (usersDto.Count() == 0)
-            {
-                return NotFound();
-            }
-
-            return Ok(usersDto);
-        }
-
-        [HttpGet("{id}")]
-        public IActionResult GetById(int id)
-        {
-            var user = _userService.GetById(id);
-
-            /* agregado -> revisar */
-            //if (user == null)
-            //{
-            //    return NotFound();
-            //}
-
-
-            // only allow admins to access other user records
-            //var currentUserId = int.Parse(User.Identity.Name);
-            //if (id != currentUserId && !User.IsInRole(Role.Admin))
-            //{
-            //    return Forbid();
-            //}
-            /* ******** */
-
-            var userDto = _mapper.Map<UserDto>(user);
-            return Ok(userDto);
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, [FromBody]UserDto userDto)
-        {
-            // map dto to entity and set id
-            var user = _mapper.Map<Usuario>(userDto);
-            user.Id = id;
-
-            try
-            {
-                // save
-                _userService.Update(user, userDto.Password);
-                return Ok();
-            }
-            catch (AppException ex)
-            {
-                // return error message if there was an exception
-                return BadRequest(new { message = ex.Message });
-            }
-        }
-
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            try
-            {
-                _userService.Delete(id);
-                return Ok();
-            }catch (AppException ex)
-            {
-                return BadRequest(new { message = ex.Message });
-            }
-            
-        }
+      // return basic user info (without password) and token to store client side
+      return Ok(new
+      {
+        Id = user.Id,
+        Username = user.Username,
+        IdRol = user.IdRol,
+        Rol = user.IdRolNavigation.Rol1,
+        IdEmpleado = user.IdEmpleado,
+        Token = tokenString
+      });
     }
+
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public IActionResult Register([FromBody]UserDto userDto)
+    {
+      // map dto to entity
+      var user = _mapper.Map<Usuario>(userDto);
+
+      try
+      {
+        // save 
+        _userService.Create(user, userDto.Password);
+        return Ok();
+      }
+      catch (AppException ex)
+      {
+        // return error message if there was an exception
+        return BadRequest(new { message = ex.Message });
+      }
+    }
+
+    [Authorize(Roles = "Admin, PM")]
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+      var users = _userService.GetAll();
+      var usersDto = _mapper.Map<IList<UserEmplDto>>(users);
+
+      if (usersDto.Count() == 0)
+        return NotFound();
+
+      return Ok(usersDto);
+    }
+
+    //[Authorize (Roles = "Admin")]
+    [AllowAnonymous]
+    [HttpGet("apynom/{dato}")]
+    public IActionResult GetByApyNom([FromRoute] string dato)
+    {
+      var users = _userService.GetByDato(dato);
+      var usersDto = _mapper.Map<IList<UserEmplDto>>(users);
+      if (usersDto.Count() == 0)
+      {
+        return NotFound();
+      }
+      return Ok(usersDto);
+    }
+
+    [AllowAnonymous]
+    [HttpGet("dni/{dni}")]
+    public IActionResult GetByDni(int dni)
+    {
+      var user = _userService.GetByDni(dni);
+      var usersDto = _mapper.Map<IList<UserEmplDto>>(user);
+
+      if (usersDto.Count() == 0)
+      {
+        return NotFound();
+      }
+
+      return Ok(usersDto);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+      var user = _userService.GetById(id);
+
+      /* agregado -> revisar */
+      //if (user == null)
+      //{
+      //    return NotFound();
+      //}
+
+
+      // only allow admins to access other user records
+      //var currentUserId = int.Parse(User.Identity.Name);
+      //if (id != currentUserId && !User.IsInRole(Role.Admin))
+      //{
+      //    return Forbid();
+      //}
+      /* ******** */
+
+      var userDto = _mapper.Map<UserDto>(user);
+      return Ok(userDto);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, [FromBody]UserDto userDto)
+    {
+      // map dto to entity and set id
+      var user = _mapper.Map<Usuario>(userDto);
+      user.Id = id;
+
+      try
+      {
+        // save
+        _userService.Update(user, userDto.Password);
+        return Ok();
+      }
+      catch (AppException ex)
+      {
+        // return error message if there was an exception
+        return BadRequest(new { message = ex.Message });
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+      try
+      {
+        _userService.Delete(id);
+        return Ok();
+      }
+      catch (AppException ex)
+      {
+        return BadRequest(new { message = ex.Message });
+      }
+
+    }
+  }
 }
--- gpro-web/Services/UsuarioService.cs
+++ gpro-web/Services/UsuarioService.cs
@@ -8,198 +8,215 @@ using gpro_web.Models;
 
 namespace gpro_web.Services
 {
-    public interface IUsuarioService
-{
-        Usuario Authenticate(string username, string password);
-        IEnumerable<Usuario> GetAll();
-        Usuario GetById(int id);
-        List<Object> GetByDato(string dato);
-        Object GetByDni(int dni);
-        Usuario Create(Usuario usuario, string password);
-        void Update(Usuario usuario, string password = null);
-        void Delete(int id);
-}
-    public class UsuarioService : IUsuarioService
-{
-        private gpro_dbContext _context;
+  public interface IUsuarioService
+  {
+    Usuario Authenticate(string username, string password);
+    List<Object> GetAll();
+    Usuario GetById(int id);
+    List<Object> GetByDato(string dato);
+    Object GetByDni(int dni);
+    Usuario Create(Usuario usuario, string password);
+    void Update(Usuario usuario, string password = null);
+    void Delete(int id);
+  }
+  public class UsuarioService : IUsuarioService
+  {
+    private gpro_dbContext _context;
 
-        public UsuarioService(gpro_dbContext context)
-        {
-            _context = context;
-        }
-
-        public Usuario Authenticate(string username, string password)
-        {
-            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
-                return null;
-
-            //var user = _context.Usuario.SingleOrDefault(x => x.Username == username && x.Password == password);
-            var user = _context.Usuario.SingleOrDefault(x => x.Username == username);
-
-            // return null if user not found
-            if (user == null)
-                return null;
-
-            // remove password before returning
-            //user.Password = null;
-
-            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
-                return null;
-
-            return user;
-        }
-
-        public IEnumerable<Usuario> GetAll()
-        {
-            return _context.Usuario;
-        }
-
-        public Usuario GetById(int id)
-        {
-            return _context.Usuario.Find(id);
-        }
-
-        public List<Object> GetByDato(string dato)
-        {
-            List<Object> usuarios = new List <Object>();
-            var empleados = from b in _context.Empleado
-                           where (b.ApellidoEmpleado.Contains(dato) || b.NombreEmpleado.Contains(dato)) && b.Usuario.Count() == 1
-                           select b;
-            if (empleados.ToList().Count == 0)
-            {
-                return null;
-            }
-            else
-            {
-                int i;
-                
-                for (i = 0; i < empleados.ToList().Count; i++)
-                {
-                    var aux = from usr in _context.Usuario
-                              where usr.IdEmpleadoNavigation.IdEmpleado.Equals(empleados.ToList().ElementAt(i).IdEmpleado)
-                              select usr;
-
-
-                   
-                    usuarios.Add(aux.ToList().ElementAt(0).IdEmpleadoNavigation);
-                }
-            }
-            return usuarios;
-        }
-
-        public Object GetByDni(int dni)
-        {
-            var empl = from b in _context.Empleado
-                          where b.Dni.Equals(dni)
-                          select b;
-
-            return empl;
-        }
-
-        public Usuario Create(Usuario usuario, string password)
-        {
-            //validation
-            if (string.IsNullOrWhiteSpace(password))
-                throw new AppException("Password is required");
-
-
-            if (_context.Usuario.Any(x => x.Username == usuario.Username))
-                throw new AppException("Username \"" + usuario.Username + "\" is already taken");
-
-            if (_context.Usuario.Any(x => x.IdEmpleado == usuario.IdEmpleado))
-                throw new AppException("Empleado \"" + usuario.IdEmpleado + "\" ya posee usuario.");
-
-            byte[] passwordHash, passwordSalt;
-            CreatePasswordHash(password, out passwordHash, out passwordSalt);
-
-            usuario.PasswordHash = passwordHash;
-            usuario.PasswordSalt = passwordSalt;
-
-            _context.Usuario.Add(usuario);
-            _context.SaveChanges();
-
-            return usuario;
-        }
-
-        public void Update(Usuario userParam, string password = null)
-        {
-            var user = _context.Usuario.Find(userParam.Id);
-
-            if (user == null)
-                throw new AppException("User not found");
-
-            if (userParam.Username != user.Username)
-            {
-                // username has changed so check if the new username is already taken
-                if (_context.Usuario.Any(x => x.Username == userParam.Username))
-                    throw new AppException("Username " + userParam.Username + " is already taken");
-            }
-
-            // update user properties
-            user.Username = userParam.Username;
-            user.IdEmpleado = userParam.IdEmpleado;
-            user.IdRol = userParam.IdRol;
-
-            // update password if it was entered
-            if (!string.IsNullOrWhiteSpace(password))
-            {
-                byte[] passwordHash, passwordSalt;
-                CreatePasswordHash(password, out passwordHash, out passwordSalt);
-
-                user.PasswordHash = passwordHash;
-                user.PasswordSalt = passwordSalt;
-            }
-
-            _context.Usuario.Update(user);
-            _context.SaveChanges();
-        }
-
-        public void Delete(int id)
-        {
-            var user = _context.Usuario.Find(id);
-            if (user == null)
-            {
-                throw new AppException("El id " + id + " no se existe.");
-            }
-            
-            _context.Usuario.Remove(user);
-            _context.SaveChanges();
-
-        }
-
-
-        //private helper methods
-
-        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-        {
-            if (password == null) throw new ArgumentNullException("password");
-            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
-
-            using (var hmac = new System.Security.Cryptography.HMACSHA512())
-            {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
-        }
-
-        private static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
-        {
-            if (password == null) throw new ArgumentNullException("password");
-            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
-            if (storedHash.Length != 64) throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
-            if (storedSalt.Length != 128) throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordHash");
-
-            using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
-            {
-                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-                for (int i = 0; i < computedHash.Length; i++)
-                {
-                    if (computedHash[i] != storedHash[i]) return false;
-                }
-            }
-
-            return true;
-        }
+    public UsuarioService(gpro_dbContext context)
+    {
+      _context = context;
     }
+
+    public Usuario Authenticate(string username, string password)
+    {
+      if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+        return null;
+
+      //var user = _context.Usuario.SingleOrDefault(x => x.Username == username && x.Password == password);
+      var user = _context.Usuario.SingleOrDefault(x => x.Username == username);
+
+      // return null if user not found
+      if (user == null)
+        return null;
+
+      // remove password before returning
+      //user.Password = null;
+
+      if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+        return null;
+
+      return user;
+    }
+
+    public List<Object> GetAll()
+    {
+      List<Object> usuarios = new List<Object>();
+      var _usuarios = from u in _context.Usuario select u;
+
+      if (_usuarios.ToList().Count() == 0)
+      {
+        return null;
+      }
+      else
+      {
+        for (int i = 0; i < _usuarios.ToList().Count; i++)
+        {
+          var aux = from usr in _context.Usuario
+                    where usr.IdEmpleadoNavigation.IdEmpleado.Equals(_usuarios.ToList().ElementAt(i).IdEmpleado)
+                    select usr;
+          usuarios.Add(aux.ToList().ElementAt(0).IdEmpleadoNavigation);
+        }
+      }
+
+      return usuarios;
+    }
+
+    public Usuario GetById(int id)
+    {
+      return _context.Usuario.Find(id);
+    }
+
+    public List<Object> GetByDato(string dato)
+    {
+      List<Object> usuarios = new List<Object>();
+      var empleados = from b in _context.Empleado
+                      where (b.ApellidoEmpleado.Contains(dato) || b.NombreEmpleado.Contains(dato)) && b.Usuario.Count() == 1
+                      select b;
+      if (empleados.ToList().Count == 0)
+      {
+        return null;
+      }
+      else
+      {
+        int i;
+
+        for (i = 0; i < empleados.ToList().Count; i++)
+        {
+          var aux = from usr in _context.Usuario
+                    where usr.IdEmpleadoNavigation.IdEmpleado.Equals(empleados.ToList().ElementAt(i).IdEmpleado)
+                    select usr;
+
+
+
+          usuarios.Add(aux.ToList().ElementAt(0).IdEmpleadoNavigation);
+        }
+      }
+      return usuarios;
+    }
+
+    public Object GetByDni(int dni)
+    {
+      var empl = from b in _context.Empleado
+                 where b.Dni.Equals(dni)
+                 select b;
+
+      return empl;
+    }
+
+    public Usuario Create(Usuario usuario, string password)
+    {
+      //validation
+      if (string.IsNullOrWhiteSpace(password))
+        throw new AppException("Password is required");
+
+
+      if (_context.Usuario.Any(x => x.Username == usuario.Username))
+        throw new AppException("Username \"" + usuario.Username + "\" is already taken");
+
+      if (_context.Usuario.Any(x => x.IdEmpleado == usuario.IdEmpleado))
+        throw new AppException("Empleado \"" + usuario.IdEmpleado + "\" ya posee usuario.");
+
+      byte[] passwordHash, passwordSalt;
+      CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+      usuario.PasswordHash = passwordHash;
+      usuario.PasswordSalt = passwordSalt;
+
+      _context.Usuario.Add(usuario);
+      _context.SaveChanges();
+
+      return usuario;
+    }
+
+    public void Update(Usuario userParam, string password = null)
+    {
+      var user = _context.Usuario.Find(userParam.Id);
+
+      if (user == null)
+        throw new AppException("User not found");
+
+      if (userParam.Username != user.Username)
+      {
+        // username has changed so check if the new username is already taken
+        if (_context.Usuario.Any(x => x.Username == userParam.Username))
+          throw new AppException("Username " + userParam.Username + " is already taken");
+      }
+
+      // update user properties
+      user.Username = userParam.Username;
+      user.IdEmpleado = userParam.IdEmpleado;
+      user.IdRol = userParam.IdRol;
+
+      // update password if it was entered
+      if (!string.IsNullOrWhiteSpace(password))
+      {
+        byte[] passwordHash, passwordSalt;
+        CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+        user.PasswordHash = passwordHash;
+        user.PasswordSalt = passwordSalt;
+      }
+
+      _context.Usuario.Update(user);
+      _context.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+      var user = _context.Usuario.Find(id);
+      if (user == null)
+      {
+        throw new AppException("El id " + id + " no se existe.");
+      }
+
+      _context.Usuario.Remove(user);
+      _context.SaveChanges();
+
+    }
+
+
+    //private helper methods
+
+    private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+      if (password == null) throw new ArgumentNullException("password");
+      if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+
+      using (var hmac = new System.Security.Cryptography.HMACSHA512())
+      {
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+      }
+    }
+
+    private static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+    {
+      if (password == null) throw new ArgumentNullException("password");
+      if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+      if (storedHash.Length != 64) throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
+      if (storedSalt.Length != 128) throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordHash");
+
+      using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
+      {
+        var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+          if (computedHash[i] != storedHash[i]) return false;
+        }
+      }
+
+      return true;
+    }
+  }
 }
-    
